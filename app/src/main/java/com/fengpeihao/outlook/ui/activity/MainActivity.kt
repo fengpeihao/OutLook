44016c15e1c6a063c179c1fc3aadac3207ea780b
peihao.feng <fengpeihao@greendotcorp.com>
2021-12-21 14:21:08 +0800

    Initial commit

---
+++ app/src/main/java/com/fengpeihao/outlook/ui/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fengpeihao.outlook.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fengpeihao.outlook.R
+
+/**
+ * @description
+ * @author: created by peihao.feng
+ * @date: 2021/12/21
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
